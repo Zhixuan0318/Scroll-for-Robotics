18df1c0311fe5f4d7478ace5a588cb7b6b0c13ad
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2025-02-20 16:17:49 +0800

    add data

---
+++ src/data/logs.ts
@@ -0,0 +1,10 @@
+export default [
+    'Warehouse Processing', // placeOrder
+    'Warehouse Processed', // processOrder
+    'Product Picking', // generateRobotId(0)
+    'Product Picked', // pickOrder
+    'Product Packing', // generateRobotId(1)
+    'Product Packed', // packOrder
+    'Order Delivering', // generateRobotId(2)
+    'Order Delivered', // deliverOrder
+];
