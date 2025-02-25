ec7fe1fe275a65bb8d3c5d8d5473ce0c903eadd1
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2025-02-25 09:49:21 +0800

    update contract address

---
+++ src/data/addresses.ts
@@ -0,0 +1,10 @@
+const addresses: { [key: string]: `0x${string}` } = {
+    owner: '0x6F01939f87C681b4FC0a36127Ae0Bd7B4b5B9F40',
+    shop: '0x99355Ba19923EC8601f5A1CD8F9E3e93d13EB4C9',
+    products: '0x420fC211DB1Bfb0b0E29a49E3f51E0C806F3C0e6',
+    warehouse: '0x435c8C6048ae5601D05Dd5D42C16141602C86990',
+    picker: '0x47ed499231d767135347B5a01BB690d40fA24aAC',
+    packer: '0xbb1A5d36306Baa5AC756e2c0b2dC99BD3cab6CD2',
+    deliverer: '0x42cd8054593EF719E4540cB29Af06Ce28BdC2b3F',
+};
+export default addresses;
